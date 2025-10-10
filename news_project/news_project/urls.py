fe72459ba88c8de29e0ac625503cddcd2fee628c
jipipes <118008846+jipipes@users.noreply.github.com>
2025-10-10 13:50:35 +0900

    [Fix] 불필요 파일 삭제 및 최종수정

--- news_project/news_project/urls.py
+++ news_project/news_project/urls.py
@@ -38,10 +38,6 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
-urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
-    # ...
-]
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
